7a7d59a9731681b4c1c5104daab6a6817c25d656
dasilva128 <hasoonhandavi@gmail.com>
2025-06-09 08:20:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ ReplyDB = {}
 FormtDB = {}
 NubBot = Client(
     session_name=Config.SESSION_NAME,
-    api_id=int(Config.API_ID, 14699743),
+    api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN
 )
